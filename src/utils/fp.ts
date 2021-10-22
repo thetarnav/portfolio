1cca07d1a2960ce6dd6ac833096f9831b110f366
thetarnav <gthetarnav@gmail.com>
2021-10-22 17:10:23 +0200

    Clear astro project files

--- src/utils/fp.ts
+++
@@ -1,6 +0,0 @@
-export const copyArray = <T>(list: readonly T[]): T[] => list.slice()
-
-export const copyObject = <T extends Record<any, any>>(object: T): T =>
-	Object.assign({}, object)
-
-export const minus = (a: number, b: number): number => a - b
